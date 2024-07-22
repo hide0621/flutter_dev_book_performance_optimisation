d4f9b6c65319bde46de235c4b572dc73d80967b6
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-07-23 08:49:50 +0900

    ドキュメントコメントを追加

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// [findAncestorWidgetOfExactType]
+    /// 現在のウィジェットから見て最も近い親方向にある、指定されたタイプTのウィジェットインスタンスを返す
+    /// もし指定されたタイプのウィジェットが見つからなければ、nullを返す
     final materialApp = context.findAncestorWidgetOfExactType<MaterialApp>();
     print(materialApp); // MaterialApp
 
