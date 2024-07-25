55202d2740b4e3e00c8cd9667892b3aac44989ea
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-07-26 08:53:44 +0900

    StatefulWidgetとStatefulWidgetのStateのライフサイクルの違いを表すコードを書いてみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: HomeScreen(),
   ));
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+//
+//   /// BuildContext = Elementというクラス
+//   @override
+//   Widget build(BuildContext context) {
+//     /// [findAncestorWidgetOfExactType]
+//     /// 現在のウィジェットから見て最も近い親方向にある、指定されたタイプTのウィジェットインスタンスを返す
+//     /// もし指定されたタイプのウィジェットが見つからなければ、nullを返す
+//     final materialApp = context.findAncestorWidgetOfExactType<MaterialApp>();
+//     print(materialApp); // MaterialApp
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home Screen'),
+//       ),
+//       body: const Center(
+//         child: Text('Home Screen'),
+//       ),
+//     );
+//   }
+// }
 
-  /// BuildContext = Elementというクラス
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key}) {
+    debugPrint('HomeScreen constructor');
+  }
+
+  @override
+  State createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    /// [findAncestorWidgetOfExactType]
-    /// 現在のウィジェットから見て最も近い親方向にある、指定されたタイプTのウィジェットインスタンスを返す
-    /// もし指定されたタイプのウィジェットが見つからなければ、nullを返す
-    final materialApp = context.findAncestorWidgetOfExactType<MaterialApp>();
-    print(materialApp); // MaterialApp
-
+    debugPrint('CounterButton build');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
-      body: const Center(
-        child: Text('Home Screen'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Home Screen Count: ($_counter)'),
+              onPressed: () {
+                setState(() {
+                  _counter++;
+                });
+              },
+            ),
+            CounterButton(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class CounterButton extends StatefulWidget {
+  CounterButton({super.key}) {
+    debugPrint('CounterButton constructor');
+  }
+
+  @override
+  State createState() => _CounterButtonState();
+}
+
+class _CounterButtonState extends State<CounterButton> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('CounterButton build');
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          _counter++;
+        });
+      },
+      child: Text('Counter Button Count: ($_counter)'),
     );
   }
 }
